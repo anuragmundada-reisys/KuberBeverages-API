224907cd553c4b87667c80c6abea7e0bca80c48a
Anurag Mundada <anurag.mundada@NW0C6FPW9T.local>
2022-03-01 12:50:15 -0500

    fix cors issue

--- src/main/java/com/kuber/config/WebSecurityConfig.java
+++ src/main/java/com/kuber/config/WebSecurityConfig.java
@@ -10,8 +10,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
         http.requiresChannel()
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
+                .requiresSecure()
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin().addHeaderWriter(((request, response) -> {
+                    response.setHeader("Cache-Control","no-cache, no-store, max-age=0, must-revalidate, private");
+                    response.setHeader("Pragma","no-cache");
+                    response.setHeader("Access-Control-Allow-Origin","*");
+                    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-Forwarded-Proto, Content-Type, Accept, Cache-Control, Pragma, Expires, X-Auth-Token");
+                }));
     }
 }
